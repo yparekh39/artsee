8438162c6e8010a52f8f3974c36f434e68a7b83b
Tahrima <tahrimatasmin@gmail.com>
2015-04-19 04:06:49 -0400

    Getting project info from Behance API

--- script.js
+++ script.js
@@ -11,17 +11,30 @@ var rand_int = (Math.floor(Math.random()*10)+1);
 		}
 	be(behance_api_key).project.search(query, function success(results2){
 		be(behance_api_key).project(results2.projects[0].id, function success(results3){
-			//console.log(results3);
+			console.log(results3);
 			var i = 0;
 			while(results3.project.modules[i].type != "image"){i++;}
 			image = results3.project.modules[i].src;
-			//console.log(image);
+			//console.log(image);			
 			var DOM_img = document.createElement("img");
 			DOM_img.src = image;
-			DOM_img.className = "Image"
+			DOM_img.className = "Image";
 			document.body.appendChild(DOM_img);
+			
+			var header = document.createElement("h1");
+			var title = document.createTextNode(results3.project.name);
+			header.appendChild(title);
+			document.body.appendChild(header);
+			
+			var header2 = document.createElement("h2");
+			for(var i = 0; i < results3.project.owners.length; ++i){
+				var author = document.createTextNode(results3.project.owners[i].first_name+" "+ results3.project.owners[i].last_name);
+				header2.appendChild(author);
+			}
+			document.body.appendChild(header2);
 		});
 	});
+	
 	
 
 
