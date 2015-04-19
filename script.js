1dbf691803a4ef5f8667acb0dc000da8f52fa214
Yash Parekh <yparekh39@gmail.com>
2015-04-18 21:47:43 -0400

    Copied working code from test branch

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 var behance_api_key = "kVdG80vj5LranYUY2MzSu5cge0GTLsR7";
 var image;
-be(behance_api_key).project('4889175', function success(results){
-	image = results.project.modules[0].src;
-	//console.log(image);
-	console.log(image);
-	var DOM_img = document.createElement("img");
-	DOM_img.src = image;
-	DOM_img.className = "Image"
-	document.body.appendChild(DOM_img);
-	//results.project.covers.202
+var rand_int = (Math.floor(Math.random()*66)+1);
+
+
+be(behance_api_key).fields(function success(results){
+	console.log(rand_int);
+	console.log(results.fields[rand_int]);
+	console.log(results.fields[rand_int]);
+	var firstField = results.fields[rand_int];
+	console.log(escape(firstField.name.toLowerCase()).replace(/%20/g, '+'));
+	var query =
+		{
+			field: escape(firstField.name.toLowerCase().replace(/%20/g, '+')),
+			//field: "fine+arts",
+			page: "2"
+		}
+	be(behance_api_key).project.search(query, function success(results2){
+		console.log(results2);
+		
+		
+		be(behance_api_key).project(results2.projects[0].id, function success(results3){
+			console.log(results3);
+			image = results3.project.modules[0].src;
+			console.log(image);
+			var DOM_img = document.createElement("img");
+			DOM_img.src = image;
+			DOM_img.className = "Image"
+			document.body.appendChild(DOM_img);
+		});
+	});
+	
+	
 });
+
+
+
+
+
+
+
+
