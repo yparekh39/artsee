61d57fc1bf43b190782ae426f77eeb7223dfc530
Tahrima <tahrimatasmin@gmail.com>
2015-04-18 23:48:01 -0400

    Centering updates

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var rand_int = (Math.floor(Math.random()*66)+1);
 
 
 be(behance_api_key).fields(function success(results){
-	console.log(rand_int);
-	console.log(results.fields[rand_int]);
-	console.log(results.fields[rand_int]);
+	//console.log(rand_int);
+	//console.log(results.fields[rand_int]);
+	//console.log(results.fields[rand_int]);
 	var firstField = results.fields[rand_int];
-	console.log(escape(firstField.name.toLowerCase()).replace(/%20/g, '+'));
+	//console.log(escape(firstField.name.toLowerCase()).replace(/%20/g, '+'));
 	var query =
 		{
 			field: escape(firstField.name.toLowerCase().replace(/%20/g, '+')),
@@ -16,15 +16,15 @@ be(behance_api_key).fields(function success(results){
 			page: "2"
 		}
 	be(behance_api_key).project.search(query, function success(results2){
-		console.log(results2);
+		//console.log(results2);
 		
 		
 		be(behance_api_key).project(results2.projects[0].id, function success(results3){
-			console.log(results3);
+			//console.log(results3);
 			var i = 0;
 			while(results3.project.modules[i].type != "image"){i++;}
 			image = results3.project.modules[i].src;
-			console.log(image);
+			//console.log(image);
 			var DOM_img = document.createElement("img");
 			DOM_img.src = image;
 			DOM_img.className = "Image"
